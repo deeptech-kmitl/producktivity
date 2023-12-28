e2e702d36d8683594c8177aaabcdc5534be8a3d2
spicyzboss <supachai@spicyz.io>
2023-12-29 04:06:38 +0700

    feat(web): add fonts

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return <p>Produck's Certificate Generator</p>;
+  return <p>Produck's Certificate Generator สวัสดี</p>;
 });
 
 export const head: DocumentHead = {
