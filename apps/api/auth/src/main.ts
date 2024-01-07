1469a25a2faa9a67abb690df1e89201c48374845
santhitak <santhita.sk@gmail.com>
2024-01-07 14:21:25 +0700

    build: re-init api/auth

--- apps/api/auth/src/main.ts
+++ apps/api/auth/src/main.ts
@@ -1,10 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(3000);
+  const globalPrefix = 'api/v1/auth';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
