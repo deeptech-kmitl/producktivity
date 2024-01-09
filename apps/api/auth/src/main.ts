dc31a0d8122b53b6a3a95071eb49b7099bead277
skrajangwong <santhita.krajangwongpaisan@agoda.com>
2024-01-09 14:11:04 +0700

    bugs: SWC bundling config

--- apps/api/auth/src/main.ts
+++ apps/api/auth/src/main.ts
@@ -5,10 +5,13 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
   const globalPrefix = 'api/v1/auth';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = 3000;
   await app.listen(port);
+
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
