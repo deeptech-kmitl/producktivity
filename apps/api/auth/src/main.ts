720ab4f2f5a5f8af35419c59d20b29b45348a2e4
spicyzboss <supachai@spicyz.io>
2024-01-24 23:55:59 +0700

    feat(api-auth): init

--- apps/api/auth/src/main.ts
+++ apps/api/auth/src/main.ts
@@ -5,11 +5,11 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  const globalPrefix = 'api/v1/auth';
-  app.setGlobalPrefix(globalPrefix);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
