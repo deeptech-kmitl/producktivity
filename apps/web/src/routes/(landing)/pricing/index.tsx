b6fe6f47fc43d67a7eb56815832c1537f79ab885
skrajangwong <santhita.krajangwongpaisan@agoda.com>
2024-02-20 11:29:19 +0700

    feat: improve pricing card page

--- apps/web/src/routes/(landing)/pricing/index.tsx
+++ apps/web/src/routes/(landing)/pricing/index.tsx
@@ -1,14 +1,19 @@
 import { component$ } from '@builder.io/qwik';
 import { DocumentHead } from '@builder.io/qwik-city';
-import { Box, Card } from '@producktivity/ui';
+import { Box, Card, Text } from '@producktivity/ui';
 import { generateSeoConfig } from '../../../configs/seo';
 import { entPricing, hobbistPricing, proPricing } from './configs';
 
 export default component$(() => {
   return (
     <>
-      <Box gap="1" height="full" width="auto" paddingX="4">
-        <Box align="between-center" gap="4" direction="horizontal" height="full" width="full">
+      <Box paddingTop="6">
+        <Text variant="hero" themeVariant="gradient" bold>
+          Find a plan for your projects
+        </Text>
+      </Box>
+      <Box gap="1" height="full" width="auto" paddingX="4" paddingY="2">
+        <Box align="center" gap="4" direction="horizontal" height="full" width="full">
           <Card type="Hobby" price={hobbistPricing.price} desc={hobbistPricing.desc} cta={hobbistPricing.cta} features={hobbistPricing.features} />
           <Card type="Professional" price={proPricing.price} desc={proPricing.desc} cta={proPricing.cta} features={proPricing.features} />
           <Card type="Enterprise" price={entPricing.price} desc={entPricing.desc} cta={entPricing.cta} features={entPricing.features} />
--- libs/web/shared/ui/src/lib/pricing/card/card.tsx
+++ libs/web/shared/ui/src/lib/pricing/card/card.tsx
@@ -7,17 +7,18 @@ export const Card = component$((item: PricingCard) => {
   const TextVariant = {
     Hobby: 'text-blue-400',
     Professional: 'text-violet-500',
-    Enterprise: 'text-orange-500',
+    Enterprise: 'text-yellow-600',
   } satisfies { [K in PackageVariant]: string };
 
   const ButtonVariant = {
     Hobby: 'text-blue-400 border-blue-400 hover:bg-blue-400 hover:text-white',
     Professional: 'bg-violet-500 text-white hover:bg-violet-400',
-    Enterprise: 'text-orange-500 border-orange-500 hover:bg-orange-500 hover:text-white',
+    Enterprise: 'text-yellow-600 border-yellow-600 hover:bg-yellow-600 hover:text-white',
   } satisfies { [K in PackageVariant]: string };
 
   return (
-    <div class={['shadow-md rounded-md grid grid-row-10 justify-between p-4 w-2/5 max-w-2/5', item.type === 'Professional' ? 'border-2 border-violet-600 h-4/5' : 'h-3/5']}>
+    <div class={['relative shadow-md rounded-md grid grid-row-10 justify-between p-4 w-1/4 max-w-1/4 items-center', item.type === 'Professional' ? 'border-4 border-violet-600 h-4/5 max-h-4/5' : 'h-3/5 max-h-3/5']}>
+      <span class={['justify-self-center px-2 py-1 text-white bg-violet-600 rounded-md absolute -top-4 font-semibold text-xl text-center', currentVariant === 'Professional' ? 'block' : 'hidden']}>MOST POPULAR</span>
       <div class="w-full row-span-3">
         <p class={['text-2xl font-bold', TextVariant[currentVariant]]}>{item.type}</p>
         <p class={['text-4xl font-bold', TextVariant[currentVariant]]}>{item.price}</p>
@@ -33,7 +34,7 @@ export const Card = component$((item: PricingCard) => {
           </p>
         ))}
       </div>
-      <button class={['w-full rounded-sm border transition-all font-semibold', ButtonVariant[currentVariant]]}>{item.cta}</button>
+      <button class={['self-end w-full py-2 rounded-sm border transition-all font-semibold', ButtonVariant[currentVariant]]}>{item.cta}</button>
     </div>
   );
 });
