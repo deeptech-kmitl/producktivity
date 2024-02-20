c5c7f354d3d37b95675138d69aece56696e20d8b
spicyzboss <supachai@spicyz.io>
2024-02-20 09:27:34 +0700

    feat(web): add gap and circle

--- libs/web/dashboard/editor/src/lib/sidebar.tsx
+++ libs/web/dashboard/editor/src/lib/sidebar.tsx
@@ -1,7 +1,7 @@
 import { component$, useContext, $ } from '@builder.io/qwik';
 import { Box, Button } from '@producktivity/ui';
 import { Frame } from './context';
-import { Textbox, Rect } from 'fabric';
+import { Textbox, Rect, Circle } from 'fabric';
 
 export default component$(() => {
   const { frame } = useContext(Frame);
@@ -38,11 +38,28 @@ export default component$(() => {
     frame.add(square);
   });
 
+  const addCircle = $(() => {
+    const circle = new Circle({
+      width: 100,
+      height: 100,
+      top: frame.height / 2,
+      left: frame.width / 2,
+      originY: 'center',
+      originX: 'center',
+      borderScaleFactor: 2,
+      cornerColor: 'white',
+      transparentCorners: false,
+    });
+
+    frame.add(circle);
+  });
+
   return (
     <Box width="half" height="full" variant="surface" direction="horizontal">
-      <Box width="full" height="full" align="top" direction="vertical">
+      <Box width="full" height="full" align="top" direction="vertical" gap="1">
         <Button onClick$={addText}>Add Text</Button>
         <Button onClick$={addSquare}>Add Square</Button>
+        <Button onClick$={addCircle}>Add Circle</Button>
       </Box>
     </Box>
   );
