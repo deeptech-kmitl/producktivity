72c7e8f38d140cd307e625e10f5ddd14472649ba
spicyzboss <supachai@spicyz.io>
2024-02-20 10:36:49 +0700

    chore: change from eslint to nx lint

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('lint-staged').Config} */
 export default {
-  '**/*.{js,mjs,jsx,ts,tsx,vue}': 'eslint --fix',
+  '**/*.{js,mjs,jsx,ts,tsx,vue}': 'nx run-many -t lint --fix',
   '**/*.{html,json}': 'prettier --write',
 }
