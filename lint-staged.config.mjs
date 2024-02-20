be4735b34a150a3af20a89205902454b97ddfc1e
spicyzboss <supachai@spicyz.io>
2024-02-20 11:01:10 +0700

    chore: back to eslint

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('lint-staged').Config} */
 export default {
-  '**/*.{js,mjs,jsx,ts,tsx,vue}': 'nx affected -t lint -- --fix=true',
+  '**/*.{js,mjs,jsx,ts,tsx,vue}': 'eslint --fix',
   '**/*.{html,json}': 'prettier --write',
 }
