d646c0f1628a342935b43a8265a8fd4681701447
skrajangwong <santhita.krajangwongpaisan@agoda.com>
2024-02-20 17:19:17 +0700

    feat: apply color theme

--- libs/web/shared/ui/src/lib/pricing/card/card.tsx
+++ libs/web/shared/ui/src/lib/pricing/card/card.tsx
@@ -5,20 +5,20 @@ export const Card = component$((item: PricingCard) => {
   const currentVariant = item.type;
 
   const TextVariant = {
-    Hobby: 'text-blue-400',
-    Professional: 'text-violet-500',
-    Enterprise: 'text-yellow-600',
+    Hobby: 'text-primary',
+    Professional: 'text-tertiary',
+    Enterprise: 'text-secondary',
   } satisfies { [K in PackageVariant]: string };
 
   const ButtonVariant = {
-    Hobby: 'text-blue-400 border-blue-400 hover:bg-blue-400 hover:text-white',
-    Professional: 'bg-violet-500 text-white hover:bg-violet-400',
-    Enterprise: 'text-yellow-600 border-yellow-600 hover:bg-yellow-600 hover:text-white',
+    Hobby: 'text-primary bg-primary-on border-primary hover:bg-primary hover:text-primary-on',
+    Professional: 'bg-tertiary text-tertiary-on hover:bg-tertiary/[.80]',
+    Enterprise: 'text-secondary bg-secondary-on border-secondary hover:bg-secondary hover:text-secondary-on',
   } satisfies { [K in PackageVariant]: string };
 
   return (
-    <div class={['relative shadow-md rounded-md grid grid-row-10 justify-between p-4 w-1/4 max-w-1/4 items-center', item.type === 'Professional' ? 'border-4 border-violet-600 h-4/5 max-h-4/5' : 'h-3/5 max-h-3/5']}>
-      <span class={['justify-self-center px-2 py-1 text-white bg-violet-600 rounded-md absolute -top-4 font-semibold text-xl text-center', currentVariant === 'Professional' ? 'block' : 'hidden']}>MOST POPULAR</span>
+    <div class={['bg-primary-on relative shadow-md rounded-md grid grid-row-10 justify-between p-4 w-1/4 max-w-1/4 items-center', item.type === 'Professional' ? 'border-4 border-tertiary h-4/5 max-h-4/5' : 'h-3/5 max-h-3/5']}>
+      <span class={['justify-self-center px-2 py-1 text-tertiary-on bg-tertiary rounded-md absolute -top-4 font-semibold text-xl text-center', currentVariant === 'Professional' ? 'block' : 'hidden']}>MOST POPULAR</span>
       <div class="w-full row-span-3">
         <p class={['text-2xl font-bold', TextVariant[currentVariant]]}>{item.type}</p>
         <p class={['text-4xl font-bold', TextVariant[currentVariant]]}>{item.price}</p>
