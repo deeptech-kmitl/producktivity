da53c9bcf3d951a71240c4829ba213040ae9dc87
skrajangwong <santhita.krajangwongpaisan@agoda.com>
2024-02-20 23:57:18 +0700

    feat(refactor): apply ui props to auth page

--- apps/web/src/routes/(authentication)/sign-up/index.tsx
+++ apps/web/src/routes/(authentication)/sign-up/index.tsx
@@ -1,47 +1,42 @@
 import { component$ } from '@builder.io/qwik';
 import { DocumentHead } from '@builder.io/qwik-city';
-import { Box, Button, Text } from '@producktivity/ui';
+import { Box, Button, Text, TextInput } from '@producktivity/ui';
 import { generateSeoConfig } from '../../../configs/seo';
 
 export default component$(() => {
   return (
     <>
       <Box width="full" height="full" style={{ display: 'flex', flexDirection: 'row', height: '100vh' }}>
-        <Box gap="1" width="full" height="full" align="center">
-          <Text variant="title" weight="bold">
+        <Box gap="1" width="full" height="full" align="center" variant="gradient">
+          <Text variant="title" weight="bold" theme="surface">
             Welcome to Produck!
           </Text>
-          <Text variant="h3">Already have account?</Text>
-          <Button href="/sign-in">Sign in</Button>
+          <Text variant="h3" theme="surface">
+            Already have account?
+          </Text>
+          <Button href="/sign-in" variant="surface" rounded="base">
+            Sign in
+          </Button>
         </Box>
 
-        <Box gap="1" width="full" height="full">
+        <Box width="full" height="full" align="center" gap="1">
           <Text variant="title" weight="bold" style={{ alignSelf: 'center' }}>
             CREATE ACCOUNT
           </Text>
-          <Text variant="h3" style={{ alignSelf: 'center' }}>
+          <Text variant="h4" theme="secondary">
             Register your account
           </Text>
-          <Box width="full" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <Box width="half">
-              <label>Name</label>
-              <input type="text" style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '5px', paddingLeft: '10px', width: '100%' }} />
-            </Box>
+
+          <Box align="center" gap="1">
+            <TextInput width="24" type="text" name="name" label="Name" rounded="full" required />
+            <TextInput width="24" type="email" name="e-mail" label="E-mail" rounded="full" required />
+            <TextInput width="24" type="password" name="password" label="Password" rounded="full" required />
           </Box>
-          <Box width="full" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <Box width="half">
-              <label>E-mail</label>
-              <input width="full" type="text" style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '5px', paddingLeft: '10px', width: '100%' }} />
-            </Box>
-          </Box>
-          <Box width="full" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <Box width="half">
-              <label>Password</label>
-              <input width="full" type="password" style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '5px', paddingLeft: '10px', width: '100%' }} />
-            </Box>
-          </Box>
-          <Box width="full" align="center">
-            <Button href="/sign-up">Sign up</Button>
+
+          <Box gap="1" width="full" align="center">
+            <Button href="/sign-in" rounded="base">
+              Sign Up
+            </Button>
           </Box>
         </Box>
       </Box>
