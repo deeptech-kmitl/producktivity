d8bd94303893ea323ff7c28ebd610efcebb51977
santhitak <santhita.sk@gmail.com>
2024-03-15 22:24:29 +0700

    feat: user di token

---
+++ libs/api/shared/domain/src/di/userDiToken.ts
@@ -0,0 +1,5 @@
+export class UserDITokens {
+  public static readonly CreateUserUseCase: unique symbol = Symbol('CreateUserUseCase');
+
+  public static readonly UserRepository: unique symbol = Symbol('UserRepository');
+}
