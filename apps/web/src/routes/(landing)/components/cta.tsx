0723c964c3c236538b2399380b7683164ae38a8a
santhitak <santhita.sk@gmail.com>
2024-03-17 19:52:15 +0700

    feat: redesign cta

--- apps/web/src/routes/(landing)/components/cta.tsx
+++ apps/web/src/routes/(landing)/components/cta.tsx
@@ -1,13 +1,24 @@
 import { Box, Button, Text } from '@producktivity/ui';
 import { component$ } from '@builder.io/qwik';
+import { LuArrowRightCircle } from '@qwikest/icons/lucide';
 
 export const CTA = component$(() => {
   return (
-    <Box gap="2" height="quarter" width="full" align="center" paddingY="8">
-      <Text weight="bold" variant="hero" theme="gradient" paddingY="1">
-        Ready to get started?
-      </Text>
-      <Button rounded="base">Create Your Certificate</Button>
+    <Box paddingX="10" paddingY="4" width="full">
+      <Box gap="2" width="full" align="center" paddingY="6" variant="primary" rounded="md">
+        <Text weight="bold" variant="hero" theme="primary">
+          Start your journey with us
+        </Text>
+        <Text>See all advantages of our application</Text>
+        <Button rounded="base" variant="tertiary" href="/pricing">
+          <Box direction="horizontal" gap="0.5" align="center">
+            <Text>Continue</Text>
+            <Text>
+              <LuArrowRightCircle />
+            </Text>
+          </Box>
+        </Button>
+      </Box>
     </Box>
   );
 });
