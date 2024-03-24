45a9b8fe0fcfa9c210f825ff7a45f7697ef60414
skrajangwong <santhita.krajangwongpaisan@agoda.com>
2024-03-24 22:21:23 +0700

    feat: add route

--- libs/web/landing/navigation/src/lib/navigation.tsx
+++ libs/web/landing/navigation/src/lib/navigation.tsx
@@ -1,9 +1,10 @@
-import { component$, useComputed$ } from '@builder.io/qwik';
+import { Fragment, component$, useComputed$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 import { Box, Navigation, Text } from '@producktivity/ui';
 
 interface NavItemProps {
   id: number;
+  isRequireAuth: boolean;
   label: string;
   link: string;
 }
@@ -13,19 +14,28 @@ const NavItems: NavItemProps[] = [
     id: 0,
     label: 'Pricing',
     link: '/pricing',
+    isRequireAuth: false,
   },
   {
     id: 1,
-    label: 'Contact',
-    link: '/contact',
+    label: 'Dashboard',
+    link: '/dashboard',
+    isRequireAuth: true,
   },
 ];
+
+interface sessionProps {
+  username: string;
+}
 
 export default component$(() => {
   const loc = useLocation();
 
+  const session: sessionProps = {
+    username: 'JackiesxD',
+  };
+
   const currentActivePath = useComputed$(() => {
-    console.log(loc.url.pathname);
     return loc.url.pathname;
   });
 
@@ -41,7 +51,7 @@ export default component$(() => {
       <Box direction="horizontal" gap="1">
         {NavItems.map((item: NavItemProps) => (
           <Navigation.Item prefetch key={item.id} href={item.link}>
-            <Text weight={currentActivePath.value === item.link ? 'semibold' : 'normal'}>{item.label}</Text>
+            {session.username !== '' ? <Text weight={currentActivePath.value === item.link ? 'semibold' : 'normal'}>{item.label}</Text> : <Fragment>{!item.isRequireAuth && <Text weight={currentActivePath.value === item.link ? 'semibold' : 'normal'}>{item.label}</Text>}</Fragment>}
           </Navigation.Item>
         ))}
       </Box>
