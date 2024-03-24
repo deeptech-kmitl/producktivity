45d1afcf9dfb3ee9b91d0342eea817d14e300da3
skrajangwong <santhita.krajangwongpaisan@agoda.com>
2024-03-24 22:03:59 +0700

    feat: cta section

--- apps/web/src/routes/(landing)/components/cta.tsx
+++ apps/web/src/routes/(landing)/components/cta.tsx
@@ -6,14 +6,14 @@ export const CTA = component$(() => {
   return (
     <Box paddingX="10" paddingY="4" width="full">
       <Box gap="2" width="full" align="center" paddingY="6" variant="primary" rounded="md">
-        <Text weight="bold" variant="hero" theme="primary">
+        <Text theme="primary" variant="title" weight="bold">
           Start your journey with us
         </Text>
-        <Text>See all advantages of our application</Text>
+        <Text theme="primary">See all advantages of our application</Text>
         <Button rounded="base" variant="tertiary" href="/pricing">
           <Box direction="horizontal" gap="0.5" align="center">
-            <Text>Continue</Text>
-            <Text>
+            <Text theme="primary">Continue</Text>
+            <Text theme="primary">
               <LuArrowRightCircle />
             </Text>
           </Box>
