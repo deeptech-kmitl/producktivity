aeefbaa4d9dc4dced0833ed1a01046186ed02bda
Santhita Krajangwongpaisan <74261236+santhitak@users.noreply.github.com>
2024-03-27 00:06:25 +0700

    feat: Initialize dashboard page (#51)
    * feat: add nav and footer to layout
    
    * feat: init tabs
    
    * feat: add components to dashboard page

---
+++ apps/web/src/routes/dashboard/components/project-tab.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+import { Box } from '@producktivity/ui';
+
+export const ProjectTab = component$(() => {
+  return (
+    <Box width="full">
+      <Box padding="2">project tab</Box>
+    </Box>
+  );
+});
--- apps/web/src/routes/dashboard/layout.tsx
+++ apps/web/src/routes/dashboard/layout.tsx
@@ -1,10 +1,14 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { Box } from '@producktivity/ui';
+import { Footer } from '@producktivity/web-landing-footer';
+import { Navigation } from '@producktivity/web-landing-navigation';
 
 export default component$(() => {
   return (
-    <Box align="center" height="full">
+    <Box height="full" width="full" align="top">
+      <Navigation />
       <Slot />
+      <Footer />
     </Box>
   );
 });
