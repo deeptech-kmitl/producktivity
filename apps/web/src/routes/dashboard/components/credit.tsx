7768d581ee8edcdd4da2c8a36916f719a90ba37b
santhitak <santhita.sk@gmail.com>
2024-03-27 18:11:57 +0700

    feat: dashboard tab

---
+++ apps/web/src/routes/dashboard/components/credit.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+import { Box } from '@producktivity/ui';
+
+export const Credit = component$(() => {
+  return (
+    <Box width="full" variant="surface">
+      <Box padding="2">Credit</Box>
+    </Box>
+  );
+});
--- apps/web/src/routes/dashboard/components/dashboard-tab.tsx
+++ apps/web/src/routes/dashboard/components/dashboard-tab.tsx
@@ -1,10 +1,80 @@
 import { component$ } from '@builder.io/qwik';
-import { Box } from '@producktivity/ui';
+import { Box, Text } from '@producktivity/ui';
+import { Credit } from './credit';
+
+interface TaskMemberProps {
+  name: string;
+}
+
+type TaskStatus = 'in progress' | 'failed' | 'success';
+
+interface TaskProps {
+  title: string;
+  status: TaskStatus;
+  member: TaskMemberProps[];
+  createdWhen: Date;
+  finishedWhen?: Date;
+}
+
+const MockDashboardTasks: TaskProps[] = [
+  {
+    title: 'KMITL Volunteering',
+    status: 'success',
+    createdWhen: new Date('11-02-2024'),
+    member: [
+      {
+        name: 'Boonpongkrong Narongrich',
+      },
+    ],
+  },
+  {
+    title: 'IT Open house participate',
+    status: 'in progress',
+    createdWhen: new Date(),
+    member: [
+      {
+        name: 'Rafah Pipatpong',
+      },
+      {
+        name: 'Sainan Anannarongdech',
+      },
+      {
+        name: 'Boonpradab Narongrich',
+      },
+    ],
+  },
+];
 
 export const DashboardTab = component$(() => {
   return (
-    <Box width="full">
-      <Box padding="2">dashboard tab</Box>
+    <Box paddingX="4" direction="horizontal" width="full" align="top">
+      <Box paddingX="4" paddingY="2" width="3/4" variant="surface">
+        <Box direction="horizontal" width="full" align="between-center">
+          <Box>
+            <Text variant="title" weight="bold">
+              Last Tasks
+            </Text>
+            <Text theme="secondary">{MockDashboardTasks ? MockDashboardTasks.length : 0} Total</Text>
+          </Box>
+          <Box direction="horizontal" gap="1">
+            <Box align="center">
+              <Text variant="title" weight="medium">
+                {MockDashboardTasks.filter((task) => task.status === 'success').length}
+              </Text>
+              <Text theme="secondary">Done</Text>
+            </Box>
+            <Box align="center">
+              <Text variant="title" weight="medium">
+                {MockDashboardTasks.filter((task) => task.status === 'success').length}
+              </Text>
+              <Text theme="secondary">In Progress</Text>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box paddingX="4" width="quarter">
+        <Credit />
+      </Box>
     </Box>
   );
 });
