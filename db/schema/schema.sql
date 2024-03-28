9f2faf209822af629c7ffd3d84fedf9cd2ade3f5
spicyzboss <supachai@spicyz.io>
2024-03-29 02:05:10 +0700

    feat: add projects and templates api

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -39,3 +39,37 @@ CREATE TABLE IF NOT EXISTS oauth_account (
     FOREIGN KEY (user_id) REFERENCES user (id),
     FOREIGN KEY (oauth_provider_id) REFERENCES oauth_provider (id)
 );
+
+CREATE TABLE IF NOT EXISTS template (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER,
+    deleted_at INTEGER,
+
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE IF NOT EXISTS project (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    template_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER,
+    deleted_at INTEGER,
+
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (template_id) REFERENCES template (id)
+);
+
+CREATE TABLE IF NOT EXISTS generate (
+    id TEXT PRIMARY KEY,
+    project_id TEXT NOT NULL,
+    args TEXT,
+    created_at INTEGER NOT NULL,
+    deleted_at INTEGER,
+
+    FOREIGN KEY (project_id) REFERENCES project (id)
+);
