f14e787ed59f8398b2ab38f19b0350ce225d323d
Santhita Krajangwongpaisan <74261236+santhitak@users.noreply.github.com>
2024-03-29 05:16:10 +0700

    tab template (#63)
    * feat(web): Add privacy and terms page
    
    * feat: Add template tab
    
    * feat: Navigate to template
    
    * feat: Navigate to create template
    
    * fix: remove orphan page
    
    * fix: remove orphan page
    
    ---------
    
    Co-authored-by: SoNicBoom24 <boomzahrk@gmail.com>

--- apps/web/src/routes/dashboard/components/template-tab.tsx
+++ apps/web/src/routes/dashboard/components/template-tab.tsx
@@ -1,10 +1,64 @@
 import { component$ } from '@builder.io/qwik';
-import { Box } from '@producktivity/ui';
+import { Box, Text, Button } from '@producktivity/ui';
+
+export interface TemplateProps {
+  id: number;
+  title: string;
+  lastEdit: Date;
+  img: string;
+}
+const MockTemplate: TemplateProps[] = [
+  {
+    id: 0,
+    title: 'Green Certificate',
+    lastEdit: new Date('02-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+  {
+    id: 1,
+    title: 'Royal Blue Certificate',
+    lastEdit: new Date('03-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+  {
+    id: 2,
+    title: 'Navy Blue and Gold Certificate',
+    lastEdit: new Date('04-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+  {
+    id: 3,
+    title: 'Navy Blue and Gold Certificate',
+    lastEdit: new Date('04-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+];
 
 export const TemplateTab = component$(() => {
   return (
-    <Box width="full">
-      <Box padding="2">template tab</Box>
+    <Box padding="4" gap="4" width="full">
+      <Text variant="title" weight="bold">
+        Recent Templates
+      </Text>
+
+      <Box gridCols="3" gap="2">
+        {MockTemplate.map((template) => (
+          <Button variant="surface" rounded="md" key={template.id} href={`/dashboard/template/${template.id}`}>
+            <Box gap="0.5">
+              <Box padding="3" variant="surface" rounded="md">
+                <img src={template.img} width="500" height="500" alt={template.title}></img>
+              </Box>
+              <Text paddingY="0.5" variant="h3" weight="semibold">
+                {template.title}
+              </Text>
+
+              <Text variant="small" theme="secondary">
+                Last Edit {template.lastEdit.toLocaleDateString()}
+              </Text>
+            </Box>
+          </Button>
+        ))}
+      </Box>
     </Box>
   );
 });
