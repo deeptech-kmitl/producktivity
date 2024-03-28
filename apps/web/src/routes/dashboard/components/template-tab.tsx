6b1d9a3ab61934009ac68bc92c3cb4052bd1a61c
SoNicBoom24 <boomzahrk@gmail.com>
2024-03-29 00:00:14 +0700

    feat: Add template tab

--- apps/web/src/routes/dashboard/components/template-tab.tsx
+++ apps/web/src/routes/dashboard/components/template-tab.tsx
@@ -1,10 +1,51 @@
 import { component$ } from '@builder.io/qwik';
-import { Box } from '@producktivity/ui';
+import { Box, Text } from '@producktivity/ui';
 
+export interface TemplateProps {
+  id: number;
+  title: string;
+  lastEdit: Date;
+  img: string;
+}
+const MockTemplate: TemplateProps[] = [
+  {
+    id: 0,
+    title: 'Green Certificate',
+    lastEdit: new Date('02-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+  {
+    id: 2,
+    title: 'Royal Blue Certificate',
+    lastEdit: new Date('03-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+  {
+    id: 3,
+    title: 'Navy Blue and Gold Certificate',
+    lastEdit: new Date('04-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+];
 export const TemplateTab = component$(() => {
   return (
     <Box width="full">
-      <Box padding="2">template tab</Box>
+      <Box paddingX="4" paddingY="1">
+        <Text variant="title" weight="bold">
+          Recent Templates
+        </Text>
+        <Box width="full" paddingY="2" gridCols="3" gap="3">
+          {MockTemplate.map((template) => (
+            <Box key={template.id}>
+              <img src={template.img} width="500" height="500" alt={template.title}></img>
+              <Text paddingY="0.5" variant="h3" weight="bold">
+                {template.title}
+              </Text>
+              <Text theme="secondary">Last Edit {new Date(template.lastEdit).toLocaleDateString('en-GB')}</Text>
+            </Box>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 });
