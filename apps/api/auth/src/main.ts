bebc8ae90f733fea1892a760abe2fa6f1c52922c
Supachai Kheawjuy <supachai@spicyz.io>
2024-03-29 02:12:02 +0700

    feat: Add projects and templates API (#61)

--- apps/api/auth/src/main.ts
+++ apps/api/auth/src/main.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { D1Database } from '@cloudflare/workers-types';
+import { D1Database, R2Bucket } from '@cloudflare/workers-types';
 import { typeid } from 'typeid-js';
 
 interface ResponseWrapper<T, E> {
@@ -7,7 +7,7 @@ interface ResponseWrapper<T, E> {
   error: E;
 }
 
-const response = <T, E> (data: T, error ?: E): ResponseWrapper<T, E> => {
+const response = <T, E>(data: T, error?: E): ResponseWrapper<T, E> => {
   return {
     data,
     error
@@ -16,6 +16,7 @@ const response = <T, E> (data: T, error ?: E): ResponseWrapper<T, E> => {
 
 type Bindings = {
   DB: D1Database;
+  BUCKET: R2Bucket;
 }
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -84,7 +85,7 @@ interface CreateOAuthParams {
   oaid: string;
   provider: string;
   email: string;
-}
+};
 
 type CreateUserRequest = CreateUserParams & CreateOAuthParams;
 
@@ -154,5 +155,199 @@ app.post('/providers', async (c) => {
 
   return c.json(response(result));
 });
+
+interface Template {
+  id: string;
+  userId: string;
+  name: string;
+  createdAt: number;
+  updatedAt: number;
+  deletedAt: number;
+};
+
+type CreateTemplateParams = Pick<Template,
+  | 'name'
+  | 'userId'
+>;
+
+type FindTemplateParams = Pick<Template,
+  | 'userId'
+>;
+
+interface TemplateDataParams {
+  data: JSON;
+};
+
+type UpdateTemplateParams = Pick<Template,
+  | 'name'
+>;
+
+type CreateTemplateRequest = CreateTemplateParams & TemplateDataParams;
+type UpdateTemplateRequest = UpdateTemplateParams & TemplateDataParams;
+
+app.post('/templates', async (c) => {
+  const body = await c.req.json<CreateTemplateRequest>();
+
+  const id = typeid('tmpl').toString();
+
+  const key = ['templates', body.userId, id].join('/');
+
+  const createTemplateStatement = c.env.DB.prepare(`
+    INSERT INTO template (id, user_id, name, created_at)        
+    VALUES (?1, ?2, ?3, ?4)
+  `);
+
+  try {
+    await c.env.BUCKET.put(key, JSON.stringify(body.data));
+
+    const { results } = await createTemplateStatement.bind(id, body.userId, body.name, Date.now()).run();
+
+    return c.json(response(results));
+  } catch (e) {
+    return c.json(response(null, e));
+  }
+});
+
+app.get('/templates/:userId', async (c) => {
+  const { userId } = c.req.param() as FindTemplateParams;
+
+  const getTemplateStatement = c.env.DB.prepare(`
+    SELECT
+      t.id,
+      t.user_id 'userId',
+      t.key,
+      t.created_at 'createdAt',
+      t.updated_at 'updatedAt'
+    FROM template as t
+    WHERE t.user_id = ?1 AND t.deleted_at NOT NULL
+  `);
+
+  const { results } = await getTemplateStatement.bind(userId).all<Template>();
+
+  results.map((template) => {
+    template.id;
+  });
+
+  return c.json(response(results));
+});
+
+app.get('/templates/:userId/:id', async (c) => {
+  const { userId, id } = c.req.param();
+  const key = ['templates', userId, id].join('/');
+
+  try {
+    const objectRef = await c.env.BUCKET.get(key);
+    const data = objectRef.json();
+
+    return c.json(response(data));
+  } catch (e) {
+    return c.json(response(null, e));
+  }
+});
+
+app.put('/templates/:userId/:id', async (c) => {
+  const { userId, id } = c.req.param();
+  const { data, name } = await c.req.json() as UpdateTemplateRequest;
+
+  const key = ['templates', userId, id].join('/');
+
+  const updateTemplateStatement = c.env.DB.prepare(`
+    UPDATE template
+    SET name = ?1
+    WHERE id = ?2 AND deleted_at NOT NULL
+  `);
+
+  try {
+    await c.env.BUCKET.put(key, JSON.stringify(data));
+
+    const { results } = await updateTemplateStatement.bind(name, id).run();
+
+    return c.json(response(results));
+  } catch (e) {
+    return c.json(response(null, e));
+  }
+});
+
+interface Project {
+  id: string;
+  userId: string;
+  templateId: string;
+  name: string;
+  createdAt: number;
+  updatedAt: number;
+  deletedAt: number;
+};
+
+type CreateProjectRequest = Pick<Project,
+  | 'userId'
+  | 'templateId'
+  | 'name'
+>;
+
+type FindProjectRequest = Pick<Project,
+  | 'userId'
+>;
+
+type UpdateProjectRequest = Pick<Project,
+  | 'id'
+  | 'name'
+>;
+
+app.post('/projects', async (c) => {
+  const body = await c.req.json<CreateProjectRequest>();
+
+  const createProjectStatement = c.env.DB.prepare(`
+    INSERT INTO project (id, user_id, template_id, name, created_at)
+    VALUES (?1, ?2, ?3, ?4, ?5)
+  `);
+
+  const id = typeid('proj').toString();
+
+  const result = createProjectStatement.bind(id, body.userId, body.templateId, body.name, Date.now()).run();
+
+  return c.json(response(result));
+});
+
+app.get('/projects', async (c) => {
+  const { userId } = c.req.query() as FindProjectRequest;
+
+  const getProjectStatement = c.env.DB.prepare(`
+    SELECT
+      p.id,
+      p.user_id 'userId',
+      p.template_id 'templateId',
+      p.created_at 'createdAt',
+      p.updated_at 'updatedAt'
+    FROM project as p
+    WHERE p.user_id = ?1 AND p.deleted_at NOT NULL
+  `);
+
+  const { results } = await getProjectStatement.bind(userId).all<Project>();
+
+  return c.json(response(results));
+});
+
+app.patch('/projects', async (c) => {
+  const body = await c.req.json<UpdateProjectRequest>();
+
+  const updateProjectStatement = c.env.DB.prepare(`
+    UPDATE project
+    SET name = ?1
+    WHERE id = ?2 AND deleted_at NOT NULL
+  `);
+
+  const { results } = await updateProjectStatement.bind(body.name, body.id).run();
+
+  return c.json(response(results));
+});
+
+// interface Generate {
+//   id: string;
+//   projectId: string;
+//   key: string;
+//   args: string;
+//   createdAt: number;
+//   deletedAt: number;
+// }
 
 export default app;
