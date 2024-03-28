314516568ca2af608debedc58589e943d5d7d33e
SoNicBoom24 <boomzahrk@gmail.com>
2024-03-29 00:29:55 +0700

    feat: Navigate to template

--- apps/web/src/routes/dashboard/components/template-tab.tsx
+++ apps/web/src/routes/dashboard/components/template-tab.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { Box, Text } from '@producktivity/ui';
+import { Box, Text, Button } from '@producktivity/ui';
 
 export interface TemplateProps {
   id: number;
@@ -15,9 +15,15 @@ const MockTemplate: TemplateProps[] = [
     img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
   },
   {
-    id: 2,
+    id: 1,
     title: 'Royal Blue Certificate',
     lastEdit: new Date('03-11-2024'),
+    img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
+  },
+  {
+    id: 2,
+    title: 'Navy Blue and Gold Certificate',
+    lastEdit: new Date('04-11-2024'),
     img: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9970/live/9e4ab180-fd11-11ed-b2aa-9935735a579c.png',
   },
   {
@@ -29,20 +35,25 @@ const MockTemplate: TemplateProps[] = [
 ];
 export const TemplateTab = component$(() => {
   return (
-    <Box width="full">
+    <Box width="full" height="full">
       <Box paddingX="4" paddingY="1">
         <Text variant="title" weight="bold">
           Recent Templates
         </Text>
-        <Box width="full" paddingY="2" gridCols="3" gap="3">
+        <Box width="full" paddingY="2" gridCols="3" gap="2">
           {MockTemplate.map((template) => (
-            <Box key={template.id}>
-              <img src={template.img} width="500" height="500" alt={template.title}></img>
-              <Text paddingY="0.5" variant="h3" weight="bold">
-                {template.title}
-              </Text>
-              <Text theme="secondary">Last Edit {new Date(template.lastEdit).toLocaleDateString('en-GB')}</Text>
-            </Box>
+            <Button variant="surface" rounded="md" key={template.id} href={`/dashboard/template/${template.id}`}>
+              <Box gap="0.5">
+                <img src={template.img} alt={template.title}></img>
+                <Text paddingY="0.5" variant="h3" weight="semibold">
+                  {template.title}
+                </Text>
+
+                <Text variant="small" theme="secondary">
+                  Last Edit {new Date(template.lastEdit).toLocaleDateString('en-GB')}
+                </Text>
+              </Box>
+            </Button>
           ))}
         </Box>
       </Box>
