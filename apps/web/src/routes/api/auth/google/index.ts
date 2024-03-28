fa4f76266e4727ae5d1842f4f256d8022d7568c6
santhitak <santhita.sk@gmail.com>
2024-03-29 06:46:08 +0700

    Squashed commit of the following:
    commit 33e5e6ee18143df48f289329806b4610d6a8a52f
    Merge: f14e787 2b36977
    Author: spicyzboss <supachai@spicyz.io>
    Date:   Fri Mar 29 06:45:11 2024 +0700
    
        Merge branch 'finish-api'
    
    commit 2b369779b6e1e62b7b397d016d62c13ec637236d
    Author: spicyzboss <supachai@spicyz.io>
    Date:   Fri Mar 29 06:41:41 2024 +0700
    
        feat: placeholder patcher
    
    commit f14e787ed59f8398b2ab38f19b0350ce225d323d
    Author: Santhita Krajangwongpaisan <74261236+santhitak@users.noreply.github.com>
    Date:   Fri Mar 29 05:16:10 2024 +0700
    
        tab template (#63)
    
        * feat(web): Add privacy and terms page
    
        * feat: Add template tab
    
        * feat: Navigate to template
    
        * feat: Navigate to create template
    
        * fix: remove orphan page
    
        * fix: remove orphan page
    
        ---------
    
        Co-authored-by: SoNicBoom24 <boomzahrk@gmail.com>

--- apps/web/src/routes/api/auth/google/index.ts
+++ apps/web/src/routes/api/auth/google/index.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from '@builder.io/qwik-city';
-import { google } from 'apps/web/src/configs/auth';
+import { google } from '../../../../configs/auth';
 import { generateCodeVerifier, generateState } from 'arctic';
 
 export const onGet: RequestHandler = async ({ redirect, cookie }) => {
@@ -9,21 +9,21 @@ export const onGet: RequestHandler = async ({ redirect, cookie }) => {
     scopes: ['profile', 'email'],
   });
   
-  cookie.set("google_oauth_state", state, {
-		path: "/",
+  cookie.set('google_oauth_state', state, {
+		path: '/',
 		secure: import.meta.env.PROD,
 		httpOnly: true,
 		maxAge: [10, 'minutes'],
-		sameSite: "lax"
-	})
+		sameSite: 'lax'
+	});
 
-  cookie.set("google_oauth_code_verifier", codeVerifier, {
-		path: "/",
+  cookie.set('google_oauth_code_verifier', codeVerifier, {
+		path: '/',
 		secure: import.meta.env.PROD,
 		httpOnly: true,
 		maxAge: [10, 'minutes'],
-		sameSite: "lax"
-	})
+		sameSite: 'lax'
+	});
 
   throw redirect(302, url.toString());
 };
--- db/schema/drop.sql
+++ db/schema/drop.sql
@@ -1,3 +1,6 @@
+DROP TABLE generate;
+DROP TABLE project;
+DROP TABLE template;
 DROP TABLE oauth_account;
 DROP TABLE oauth_provider;
 DROP TABLE session;
