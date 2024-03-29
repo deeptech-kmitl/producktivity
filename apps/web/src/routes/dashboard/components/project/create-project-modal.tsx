7872601882f29cf171e600ccdb748ee829c7bb9b
santhitak <santhita.sk@gmail.com>
2024-03-29 08:25:20 +0700

    feat: integrate api project

--- apps/web/src/routes/dashboard/components/project/create-project-modal.tsx
+++ apps/web/src/routes/dashboard/components/project/create-project-modal.tsx
@@ -1,8 +1,9 @@
-import { Signal, component$, $ } from '@builder.io/qwik';
+import { Signal, component$, $, useVisibleTask$, useSignal, useTask$ } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
 import { useForm, required } from '@modular-forms/qwik';
 import { Box, TextInput, UIModal, UISelect } from '@producktivity/ui';
 import { useFormLoader } from '../../[slug]';
+import { useUser } from '../../../layout';
 
 interface CreateProjectModalProps {
   showModal: Signal<boolean>;
@@ -13,26 +14,40 @@ export type ProjectFormProps = {
   templateId: string;
 };
 
-const data = [
-  {
-    id: '1',
-    userId: '1',
-    name: 'Kmitl Template',
-  },
-  {
-    id: '2',
-    userId: '2',
-    name: 'IT Innovation',
-  },
-];
+interface dataTemplateProps {
+  data: Template[];
+}
 
 export const CreateProjectModal = component$(({ showModal }: CreateProjectModalProps) => {
+  const userSignal = useUser();
+  const templateList = useSignal<dataTemplateProps>({ data: [] });
   const [projectForm, { Form, Field }] = useForm<ProjectFormProps>({
     loader: useFormLoader(),
   });
 
-  const confirmAction = $((values: unknown) => {
-    console.log(values);
+  useTask$(async () => {
+    const temp = await fetch(`${import.meta.env.VITE_API_URL}/templates?userId=${userSignal.value.id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    templateList.value = await temp.json();
+  });
+
+  const confirmAction = $(async (values: ProjectFormProps) => {
+    await fetch(`${import.meta.env.VITE_API_URL}/projects`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userId: userSignal.value.id,
+        name: values.projectName,
+        templateId: values.templateId,
+      }),
+    }).then(() => {
+      showModal.value = false;
+    });
   });
 
   const cancelAction = $(() => {
@@ -41,13 +56,13 @@ export const CreateProjectModal = component$(({ showModal }: CreateProjectModalP
 
   return (
     <Form onSubmit$={(values) => confirmAction(values)}>
-      <UIModal width="1/3" rounded="md" cancelLabel="Cancel" confirmLabel="Create" showModal={showModal} title="Create new project" confirmAction={$((v) => confirmAction(v))} cancelAction={cancelAction}>
+      <UIModal width="1/3" rounded="md" cancelLabel="Cancel" confirmLabel="Create" showModal={showModal} title="Create new project" cancelAction={cancelAction}>
         <Box gap="1" width="full">
           <Field name="projectName" validate={required<string>('Please enter your name.')}>
             {(field, props) => <TextInput {...props} value={field.value} error={field.error} type="text" label="Project name" placeholder="John Doe" required />}
           </Field>
           <Field name="templateId" validate={required<string>('Please select the payment type.')}>
-            {(field, props) => <UISelect {...props} value={field.value} options={data} error={field.error} label="Template" placeholder="Select Template" required />}
+            {(field, props) => <UISelect {...props} value={field.value} options={templateList.value.data} error={field.error} label="Template" placeholder="Select Template" required />}
           </Field>
         </Box>
       </UIModal>
--- apps/web/src/routes/dashboard/template/[id]/index.tsx
+++ apps/web/src/routes/dashboard/template/[id]/index.tsx
@@ -2,11 +2,13 @@ import { component$ } from '@builder.io/qwik';
 import { DocumentHead, useLocation } from '@builder.io/qwik-city';
 import { Editor } from '@producktivity/web-dashboard-editor';
 import { generateSeoConfig } from '../../../../configs/seo';
+import { useUser } from '../../../layout';
 
 export default component$(() => {
   const location = useLocation();
+  const userSignal = useUser();
 
-  return <Editor id={location.params.id} />;
+  return <Editor id={location.params.id} user={userSignal.value} />;
 });
 
 export const head: DocumentHead = generateSeoConfig({ title: 'Editor' });
