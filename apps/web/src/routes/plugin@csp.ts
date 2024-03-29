4c780bd6d93bce2f0a6caeb3ee8ae038d298eaa8
Santhita Krajangwongpaisan <74261236+santhitak@users.noreply.github.com>
2024-03-29 12:02:44 +0700

    feat: remove csp (#65)

--- apps/web/src/routes/plugin@csp.ts
+++ apps/web/src/routes/plugin@csp.ts
@@ -4,19 +4,10 @@ import { isDev } from '@builder.io/qwik/build';
 export const onRequest: RequestHandler = (event) => {
   if (isDev) return;
 
-  const nonce = event.request.headers.get('cf-ray') || Date.now().toString(36);
-  event.sharedMap.set('@nonce', nonce);
+  // const nonce = event.request.headers.get('cf-ray') || Date.now().toString(36);
+  // event.sharedMap.set('@nonce', nonce);
 
-  const csp = [
-    'default-src \'self\' \'unsafe-inline\'',
-    "font-src 'self' https://fonts.gstatic.com",
-    "img-src 'self' 'unsafe-inline' data:",
-    `script-src 'self' 'unsafe-inline' https: 'nonce-${nonce}' 'strict-dynamic' https://*.certifine.spicyz.io`,
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-    `frame-src 'self' 'nonce-${nonce}' https://lottie.host`,
-    "object-src 'none'",
-    "base-uri 'self'",
-  ];
+  // const csp = ["default-src 'self' 'unsafe-inline'", "font-src 'self' https://fonts.gstatic.com", "img-src 'self' 'unsafe-inline' data:", `script-src 'self' 'unsafe-inline' https: 'nonce-${nonce}' 'strict-dynamic' https://*.certifine.spicyz.io`, "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com", `frame-src 'self' 'nonce-${nonce}' https://lottie.host`, "object-src 'none'", "base-uri 'self'"];
 
-  event.headers.set('Content-Security-Policy', csp.join('; '));
+  // event.headers.set('Content-Security-Policy', csp.join('; '));
 };
